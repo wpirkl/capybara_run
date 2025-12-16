742e84ae3c08fdfec82b01bd1f00da58ec6b2bca
Werner Pirkl <werner.pirkl@gmail.com>
2025-12-16 11:18:24 +0100

    spawn the tombstone at the previous score location.

--- src/plugin_tombstone.rs
+++ src/plugin_tombstone.rs
@@ -2,14 +2,14 @@ use std::time::Duration;
 use bevy::prelude::*;
 
 use crate::constants::*;
-use crate::model::{GameData, GameEnd, GameReset, GameState, PlayerJump};
+use crate::model::{GameData, GameReset, GameState};
 
 pub struct TombstonePlugin;
 
 impl Plugin for TombstonePlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup)
-            .add_systems(Update, move_tombstone)
+            .add_systems(FixedUpdate, move_tombstone)
             .add_observer(handle_tombstone_reset);
     }
 }
@@ -18,7 +18,7 @@ impl Plugin for TombstonePlugin {
 struct TombstoneSprite;
 
 
-#[derive(Component)]
+#[derive(Resource, Clone)]
 struct TombstoneSpritesheets { 
     texture: Handle<Image>,
     layout: Handle<TextureAtlasLayout>,
@@ -35,37 +35,63 @@ fn setup(
     let layout = TextureAtlasLayout::from_grid(UVec2::splat(240), 1, 1, None, None);
     let layout_handle = texture_atlas_layouts.add(layout);
 
-    commands.spawn((
-        TombstoneSprite,
-        Sprite {
-            image: texture_handle.clone(),
-            texture_atlas: Some(TextureAtlas {
-                    layout: layout_handle.clone(),
-                    index: 0,
-                }),
-            ..default()
-        },
-        Transform::from_xyz(PLAYER_X + 100., PLAYER_GROUND, 0.).with_scale(Vec3::splat(TILE_SCALE)),
-        TombstoneSpritesheets{
-            texture: texture_handle,
-            layout: layout_handle,
-        }
-    ));
+    // Store the spritesheets resource for spawning later
+    commands.insert_resource(TombstoneSpritesheets{
+        texture: texture_handle,
+        layout: layout_handle,
+    });
 }
 
 fn move_tombstone(
+    mut commands: Commands,
+    time: Res<Time>,
     game: Res<GameData>,
-    mut query: Query<(&mut Transform, &mut Visibility), With<TombstoneSprite>>
+    mut query: Query<(Entity, &mut Transform), With<TombstoneSprite>>
 )
 {
+    if game.game_state == GameState::Running {
+        for (entity, mut transform) in &mut query {
+            
+            let move_distance = game.velocity * time.delta_secs();
 
+            transform.translation.x -= move_distance;
+            if transform.translation.x < -WINDOW_WIDTH / 2.0 - SCALED_TILE_SIZE {
+                commands.entity(entity).despawn();
+            }
+        }
+    }
 }
 
 
 fn handle_tombstone_reset(
     _evt: On<GameReset>,
-    mut query: Query<&mut Visibility, With<TombstoneSprite>>
+    mut commands: Commands,
+    game: Res<GameData>,
+    spritesheets: Res<TombstoneSpritesheets>,
+    query: Query<Entity, With<TombstoneSprite>>
 )
 {
+    // Despawn any existing tombstones
+    for entity in &query {
+        commands.entity(entity).despawn();
+    }
 
+    // Spawn new tombstone if there is a previous score
+    if let Some(previous_score) = game.previous_score {
+
+        let spawn_x = previous_score + PLAYER_X;
+        commands.spawn((
+            TombstoneSprite,
+            Sprite {
+                image: spritesheets.texture.clone(),
+                texture_atlas: Some(TextureAtlas {
+                    layout: spritesheets.layout.clone(),
+                    index: 0,
+                }),
+                ..default()
+            },
+            Transform::from_xyz(spawn_x, PLAYER_GROUND, 0.).with_scale(Vec3::splat(TILE_SCALE)),
+        ));
+    
+    }
 }
